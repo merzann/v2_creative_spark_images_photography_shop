ccf49614a7575c975a09c1696e78a578c0330cc9
Annika Merz <annikamerz@MacBook-Air.local>
2025-03-14 02:54:35 +0000

    fix: adjust margin and position for message container to prevent messages from being half hidden under navbar

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,10 @@
-document.addEventListener("click", function(event) {
+ddocument.addEventListener("DOMContentLoaded", function () {
+    let alerts = document.querySelectorAll(".alert");
+    alerts.forEach(alert => {
+        alert.addEventListener("closed.bs.alert", function () {
+            document.querySelector("main").style.marginTop = "0";
+        });
+    });
 
     // Checks if user click outside the navbar when menu is open and closes menu if so
     var navbarToggler = document.querySelector(".navbar-toggler");
