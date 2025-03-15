a1f37a8d33d71c370e428dbb0d13d25626724948
Annika Merz <annikamerz@MacBook-Air.local>
2025-03-15 18:12:14 +0000

    style: delete duplicate vh definition from all allauth templates to fix alert messages misaligned, fix btn-close icon not displayed in alert messages, correct typo in script.js to fix navbar dropdown menu not closing correctly when clicking outside of menu

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,20 @@
-ddocument.addEventListener("DOMContentLoaded", function () {
-    let alerts = document.querySelectorAll(".alert");
-    alerts.forEach(alert => {
-        alert.addEventListener("closed.bs.alert", function () {
-            document.querySelector("main").style.marginTop = "0";
-        });
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  // Handle Bootstrap alert dismissals properly
+  let alerts = document.querySelectorAll(".alert");
 
-    // Checks if user click outside the navbar when menu is open and closes menu if so
+  // Close navbar when clicking outside of it
+  document.addEventListener("click", function (event) {
     var navbarToggler = document.querySelector(".navbar-toggler");
     var navbarCollapse = document.querySelector(".navbar-collapse");
 
-    if (!navbarCollapse.contains(event.target) && !navbarToggler.contains(event.target) && navbarCollapse.classList.contains("show")) {
+    if (navbarCollapse && navbarToggler) {
+      if (
+        !navbarCollapse.contains(event.target) &&
+        !navbarToggler.contains(event.target) &&
+        navbarCollapse.classList.contains("show")
+      ) {
         bootstrap.Collapse.getInstance(navbarCollapse).hide();
+      }
     }
+  });
 });
