8dd74280f89052b3a399b9aa93b46f5dd74c9c5c
Annika Merz <annikamerz@MacBook-Air.local>
2025-03-19 15:48:28 +0000

    feat: create gallery_page view and images_by_theme view

--- products/views.py
+++ products/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import ImageTheme, Product
 
-# Create your views here.
+
+def gallery_page(request):
+    """
+    Display all :model:`shop.ImageTheme` instances in the gallery.
+
+    **Context:**
+
+    ``themes``
+        A queryset containing all instances of :model:`shop.ImageTheme`.
+
+    **Template:**
+
+    :template:`shop/gallery_page.html`
+    """
+    themes = ImageTheme.objects.all()
+    return render(request, "shop/gallery_page.html", {"themes": themes})
+
+
+def images_by_theme(request, theme_slug):
+    """
+    Display all :model:`shop.Product` instances related to a specific
+    :model:`shop.ImageTheme`.
+
+    **Context:**
+
+    ``theme``
+        An instance of :model:`shop.ImageTheme` matching the given slug.
+    ``images``
+        A queryset containing all instances of :model:`shop.Product`
+        associated with the given theme.
+
+    **Template:**
+
+    :template:`shop/images_by_theme.html`
+    """
+    theme = get_object_or_404(ImageTheme, slug=theme_slug)
+    images = Product.objects.filter(theme=theme)
+    return render(
+        request,
+        "shop/images_by_theme.html",
+        {"theme": theme, "images": images},
+    )
