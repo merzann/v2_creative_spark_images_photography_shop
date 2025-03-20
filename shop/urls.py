800e50580bed16c92e38f2d0309a5845a18c3716
Annika Merz <annikamerz@MacBook-Air.local>
2025-03-20 02:31:35 +0000

    feat: add image_by_theme.html, add url path to shop/urls.py

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import gallery_page
+from .views import gallery_page, images_by_theme
 
 urlpatterns = [
     path('gallery/', gallery_page, name='gallery_page'),
+    path(
+        "gallery/<slug:theme_slug>/",
+        images_by_theme, name="images_by_theme",
+    ),
 ]
