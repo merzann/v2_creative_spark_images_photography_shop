c8a88c88a14a4f3cf1de28f7b58d4262f14fa139
Annika Merz <annikamerz@MacBook-Air.local>
2025-03-20 04:05:55 +0000

    add defensive programming to gallery.html and views.py and print statemnets for debugging

--- shop/models.py
+++ shop/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
-import random
 from cloudinary.models import CloudinaryField
 from products.models import Product
+import random
 
 
 class ImageTheme(models.Model):
--- shop/urls.py
+++ shop/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from .views import gallery_page, images_by_theme
 
 urlpatterns = [
-    path('gallery/', gallery_page, name='gallery_page'),
-    path(
-        "gallery/<slug:theme_slug>/",
-        images_by_theme, name="images_by_theme",
-    ),
+    path("gallery/", gallery_page, name="gallery_page"),
+    path("gallery/<slug:theme_slug>/", images_by_theme, name="images_by_theme"),
 ]
