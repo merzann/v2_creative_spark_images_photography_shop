4ffea983d9387bc998838831113e356a31c3bab3
Annika Merz <annikamerz@MacBook-Air.local>
2025-03-22 13:37:55 +0000

    maint: added second background in images_by_theme css addition to fix layout issues oon smaller non-portrait screens, fixed product_detail.html not rendering due to mismatch dash vs. underscore

--- shop/static/shop/js/shop_script.js
+++ shop/static/shop/js/shop_script.js
@@ -22,18 +22,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Handles click event for Back-to-top button
     const backToTopBtn = document.getElementById("backToTop");
+    const backToGalleryBtn = document.getElementById("backToGallery");
 
     window.addEventListener("scroll", function () {
         if (window.scrollY > 200) {
             backToTopBtn.style.opacity = "1";
             backToTopBtn.style.visibility = "visible";
+            backToGalleryBtn.style.opacity = "1";
+            backToGalleryBtn.style.visibility = "visible";
         } else {
             backToTopBtn.style.opacity = "0";
             backToTopBtn.style.visibility = "hidden";
+            backToGalleryBtn.style.opacity = "0";
+            backToGalleryBtn.style.visibility = "hidden";
         }
-    });
-
-    backToTopBtn.addEventListener("click", function () {
-        window.scrollTo({ top: 0, behavior: "smooth" });
     });
 });
--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import gallery_page, images_by_theme
+from products.views import product_detail
 
 urlpatterns = [
     path("gallery/", gallery_page, name="gallery_page"),
     path("gallery/<slug:theme_slug>/", images_by_theme, name="images_by_theme"),
+    path("<int:product_id>/", product_detail, name="product_detail"),
 ]
