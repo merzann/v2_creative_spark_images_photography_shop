571a1bb09bb56c9a122dfe25dfda9b2516a2e370
Annika Merz <annikamerz@MacBook-Air.local>
2025-03-24 06:37:11 +0000

    feat: link shopping cart to shopping cart icon in navbar, add product count for total of items placed in cart

--- bag/context_processors.py
+++ bag/context_processors.py
@@ -1,6 +1,25 @@
+from products.models import Product
+
+
 def bag_contents(request):
     return {
         'bag_item_count': 0,
         'bag_total': 0,
         'grand_total': 0,
+    }
+
+
+def bag_summary(request):
+    bag = request.session.get('bag', {})
+    total_items = 0
+
+    for item in bag.values():
+        total_items += item.get('quantity', 0)
+
+    # Pull last added product info if it exists
+    last_added = request.session.pop('last_added', None)
+
+    return {
+        'cart_count': total_items,
+        'last_added': last_added,
     }
--- my_shop/settings.py
+++ my_shop/settings.py
@@ -118,6 +118,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'bag.context_processors.bag_contents',
+                'bag.context_processors.bag_summary',
             ],
         },
     },
