c524b370abbabe56d50cab304d73c9f64875c5a5
Annika Merz <annikamerz@MacBook-Air.local>
2025-03-30 03:29:12 +0100

    fix: disbale Update button after change from Digital product to Printed product until Print Type is selected to prevent user from moving forward with shipping costs being updated first

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
-from django.views.decorators.http import require_POST
 from products.models import Product, ShippingRate, CountryVAT
 
 
